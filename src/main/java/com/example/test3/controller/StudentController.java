034246ef1c1012a1fbe8da208f78bbc60b9b8d46
Iroshan99 <ampiamarakoon@std.appsc.sab.ac.lk>
2024-10-05 18:00:46 +0530

    second commit

--- src/main/java/com/example/test3/controller/StudentController.java
+++ src/main/java/com/example/test3/controller/StudentController.java
@@ -3,10 +3,11 @@ package com.example.test3.controller;
 import com.example.test3.entity.Student;
 import com.example.test3.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.StreamingHttpOutputMessage;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -20,5 +21,33 @@ public class StudentController {
         studentService.put(student.getName(),student.getAge());
 
     }
+
+    @GetMapping(value = "/getstudent/{id}")
+    public Student getStudent(@PathVariable Long id){
+        return studentService.getStudentById(id);
+    }
+
+    @DeleteMapping("/deletestudent/{id}")
+    public void deleteStudent(@PathVariable Long id){
+        studentService.deleteStudent(id);
+
+    }
+
+    @PutMapping("/updatestudent/{id}")
+    public ResponseEntity<String> updateStudent(@PathVariable Long id,@RequestBody Student student){
+
+        studentService.updateStudent(id,student);
+
+        if(student!=null){
+            return new ResponseEntity<>("updated", HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("not updated",HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+
+
 
 }
--- src/main/java/com/example/test3/service/StudentService.java
+++ src/main/java/com/example/test3/service/StudentService.java
@@ -5,6 +5,8 @@ import com.example.test3.repository.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class StudentService {
 
@@ -22,6 +24,35 @@ public class StudentService {
         newStudent.setAge(age);
 
         studentRepo.save(newStudent);
+
+
+    }
+
+    public Student getStudentById(Long id) {
+        return studentRepo.findById(id).orElse(new Student());
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepo.deleteById(id);
+    }
+
+    public Student updateStudent(Long id, Student student) {
+
+        Optional<Student> student1=studentRepo.findById(id);
+
+        if(student1.isPresent()) {
+            Student student2 = student1.get();
+
+            student2.setName(student.getName());
+            student2.setAge(student.getAge());
+            return studentRepo.save(student2);
+
+        }
+
+        else {
+            return null;
+        }
+
 
 
     }
